afebd05af54f93155215298572744d54115dbede
NSingh913 <nirbhais626@deltalearns.ca>
2025-03-06 09:27:36 -0800

    psudocoded

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # This is a enigma machine and this will code your words into secret codes and you can also decode it
+
+# Greet the user
+
+# Ask the user to input the message 
+
+# Ask the user do they want to encrypt or decrypt a message
+
+# Ceate a function to encrypt a message
+
+# Create a function to decrypt a message
+
+ 
