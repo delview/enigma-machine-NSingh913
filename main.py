a19a215146573ad9bac9b9280eabfeb51430729e
NSingh913 <nirbhais626@deltalearns.ca>
2025-03-10 12:26:22 -0700

    tryed a function

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 # This is a enigma machine and this will code your words into secret codes and you can also decode it
 
 # Greet the user
+print("Welcome to the Enigma Machine! ")
+
+# Create a function to encrypt or decrypt any message using a Caeser cipher.
+def ceaser_cipher(text, shift, encrypt=True):
+
+    """
+    Encrypts or decrypts a message using a simple shift cipher.
+    """
+
+    result = ""  # This stroes the final encrypted or decrypted message.
+    if not encrypt:
+        shift = - shift 
 
 # Ask the user to input the message 
 
